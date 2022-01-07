e4a662f5f8d898ffb06702f749bfb8a75e11aa06
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-01-07 11:22:22 -0800

    Add Index route

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Linq;
 using Factory.Models;
 
@@ -8,6 +9,17 @@ namespace Factory.Controllers
 {
   public class MachinesController : Controller
   {
+    private readonly FactoryContext _db;
 
+    public MachinesController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Machine> model = _db.Machines.ToList();
+      return View(model);
+    }
   }
 }
