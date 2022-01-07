f01c92510f26ee4f157cdea24d399738f2ceb08e
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-01-07 08:55:59 -0800

    Add using directives, namespace, and class

---
+++ Factory/Controllers/MachinesController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Factory.Models;
+
+namespace Factory.Controllers
+{
+  public class MachinesController : Controller
+  {
+
+  }
+}
