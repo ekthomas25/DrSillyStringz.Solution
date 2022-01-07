dcca94f318354165b84659d94a01f20f5209107c
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-01-07 09:27:29 -0800

    Add properties

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -4,6 +4,16 @@ namespace Factory.Models
 {
   public class Engineer
   {
-    
+    public Engineer()
+    {
+      this.JoinEntities = new HashSet<EngineerMachine>();
+    }
+
+    public int EngineerId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Notes { get; set; }
+    public DateTime StartDate { get; set; }
+    public virtual ICollection<EngineerMachine> EngineerMachines { get; set; }
   }
 }
